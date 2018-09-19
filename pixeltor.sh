30f362a2d5e04cc3e6f40c8e63c36023b48be192
ureli <url.15.104@outlook.com>
2018-09-19 14:48:31 -0500

    Elegir ruta para guardar y arreglos menores.
    Cuado el directorio en donde se ejecuta el script no deja crear
    ficheros se pregunta dónde quiere guardar su fichero.

--- pixeltor.sh
+++ pixeltor.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-declare -r RED=$'\e[31m'; B_RED=$'\e[41m'; CYAN=$'\e[36m'; B_CYAN=$'\e[46m' \
+typeset -r RED=$'\e[31m'; B_RED=$'\e[41m'; CYAN=$'\e[36m'; B_CYAN=$'\e[46m' \
 	BLUE=$'\e[34m';	 B_BLUE=$'\e[44m'; GREEN=$'\e[32m'; B_GREEN=$'\e[42m' \
 	BLACK=$'\e[30m'; B_BLACK=$'\e[40m'; WHITE=$'\e[37m'; B_WHITE=$'\e[47m' \
 	PURPLE=$'\e[35m'; B_PURPLE=$'\e[45m'; YELLOW=$'\e[33m'; B_YELLOW=$'\e[43m' \
 	BOLD=$'\e[1m'; HIDE=$'\e[8m'; STROKE=$'\e[9m' \
 	INVERT=$'\e[7m'; ITALIC=$'\e[3m'; UNDERLINE=$'\e[4m' \
 	END=$'\e[0m'; SCRIPT="${0##*/}";
-
-alias clear='clear && printf "\ec\e[3J"'
 
 IMPREVISTO(){
 	echo -e "\r${SCRIPT}: has stopped.                      "
@@ -35,22 +33,47 @@ SOBRE_EL_USO(){
 
 	echo -e "${BOLD}AUTOR$END"
 }
+
+CLEAR() {
+	clear && printf "\ec\e[3J";
+}
+
 GRAFICAR() {
-	printf "    " # Separación del lado izq. antes de los no. de las columnas.
+	CLEAR;
+	printf "\n    "; # Separación del lado izq. antes de los no. de las columnas.
 	# N representa el no. de las columnas
 	for (( N = 1; N <= COLUMNAS; N++ )); do
-		printf "$BOLD$CYAN%-3s$END" "$N"
+		printf "$BOLD$CYAN%-3s$END" "$N";
 	done
 	# N representa el no. de las filas
 	for (( N = 1; N <= FILAS; N++ )); do
-		printf "$BOLD$CYAN\n%3s$END" "$N"
+		printf "$BOLD$CYAN\n%3s$END" "$N";
+
 		for (( COLUMNA = 0; COLUMNA < COLUMNAS; COLUMNA++ )); do
-			printf " %b" "$(eval "printf \${ARRAY_$N[$COLUMNA]}")"
+			printf " %b" "$(eval "printf \${ARRAY_$N[$COLUMNA]}")";
 		done
 	done
 
-	echo -e "\n\nColor seleccionado: \e[48;5;${PICKER:=15}m\x20\x20\e[m"
+	echo -e "\n\nColor seleccionado: \e[48;5;${PICKER:=15}m\x20\x20\e[m";
 }
+
+SAVE_FILE() {
+	printf '#!/usr/bin/env bash'$'\n''echo -e "' > "$1";
+
+	for (( NO_FILA = 1; NO_FILA <= FILAS; NO_FILA++ )); do
+		(for (( NO_COLUMNA = 0; NO_COLUMNA < COLUMNAS; NO_COLUMNA++ )); do
+			printf "%s" "$(eval "echo -n \${ARRAY_$NO_FILA[$NO_COLUMNA]//\[\]/\\\x20\\\x20}")";
+		done) >> "$1";
+
+		echo -n '\n'  >> "$1";
+	done; echo '"' >> "$1";
+
+	if [[ ! -f "$1" ]]; then
+		return 1
+	fi
+}
+
+
 # Precodigo --------------------------------------------------------------------
 trap IMPREVISTO INT SIGINT SIGTERM ABRT HUP TERM QUIT							# Regreso cuando se cancela con «CTRL + C»
 PARAMETROS="$*"																	# Variable con los parámetros establecidos en la consola
@@ -59,7 +82,7 @@ while getopts "dhr" OPCION; do
 		d)	DEBUG=true
 			;;
 		h)
-			printf '\e[8;19;159t'; clear
+			printf '\e[8;19;159t'; CLEAR
 			SOBRE_EL_USO
 			exit 0
 			;;
@@ -112,7 +135,7 @@ done
 
 while :; do
 	M_FILAS=$(tput lines);
-	[[ $M_FILAS = 0 ]] && unset M_FILAS || M_FILAS=$((M_FILAS -12));
+	[[ $M_FILAS = 0 ]] && unset M_FILAS || M_FILAS=$((M_FILAS -14));
 
 	read -rep "$(echo -e "$CYAN${BOLD}Numero de filas (HORIZONTAL):$END ")" -i ${M_FILAS:-""} FILAS;
 
@@ -125,43 +148,50 @@ while :; do
 done
 # Crea N arrays dados por la variable FILAS. A cada array se le asigna 01..N elementos dados por COLUMNAS
 for (( N_FILA = 1; N_FILA <= FILAS; N_FILA++ )); do
-	eval "declare -a ARRAY_${N_FILA}=($(printf "%s" "$(eval "echo {01..$COLUMNAS}")"))"
+	eval "typeset -a ARRAY_${N_FILA}=($(printf "%s" "$(eval "echo {01..$COLUMNAS}")"))";
 
 	for (( N_COLUMNA = 0; N_COLUMNA < COLUMNAS; N_COLUMNA++ )); do
-		eval "declare -a ARRAY_${N_FILA}[$N_COLUMNA]=\"[]\""
+		eval "typeset -a ARRAY_${N_FILA}[$N_COLUMNA]=\"[]\"";
 	done
 done
 
-clear; echo; GRAFICAR
+GRAFICAR;
+
+typeset OPCION; # Opción del menú seleccionada.
+typeset -i REDO_PICKER;
+typeset PICKER; # No se le asigna como entero ya que puede tomar el valor de «Q»
+typeset ARCHIVO
 
 while :; do
-	echo -e "\nElige una opción:\n"
-	echo -e "\tV,H) Marcar(:) Desmarcar(_) dada la «COLUMNA,FILA»"
-	echo -e "\t P ) Elegir color."
-	echo -e "\t S ) Guardar."
-	echo -e "\t Q ) Salir.\n"
+	echo -e "\nType according to option:\n";
+	echo -e "\tx,y) Check(:) Uncheck(_).";
+	echo -e "\t H ) Help.";
+	echo -e "\t P ) Pick color.";
+	echo -e "\t S ) Save.";
+	echo -e "\t Q ) Quit.\n";
 
-	read -ep "$(echo -e "$CYAN$BOLD >>>$END ")" OPCION
+	read -ep "$(echo -e "$CYAN$BOLD >>>$END ")" OPCION;
 
 	if [[ $OPCION =~ ^[0-9]+[-_:.][0-9]+$ ]]; then
 		if  ! [[ ${OPCION%[-_:.]*} -gt 0 && ${OPCION%[-_:.]*} -le $COLUMNAS ]] || ! [[ ${OPCION#*[-_:.]} -gt 0 && ${OPCION#*[-_:.]} -le $FILAS ]]; then
-			clear; echo; GRAFICAR
-			echo -e "\n${RED}Error: la coordenada «$OPCION» no existe.$END"
+			GRAFICAR;
+			echo -e "\n${RED}Coordinate «$OPCION» does not exist.$END";			## Strimg
 		else
 			if [[ ${OPCION//[0-9]/} =~ [:.] ]]; then
-				eval "declare -a ARRAY_${OPCION#*[:.]}[$((${OPCION%[:.]*}-1))]=\"\e[48;5;${PICKER}m\x20\x20\e[m\""
-				clear; echo; GRAFICAR
-				echo -e "\n${GREEN}Coordenada «$BOLD$OPCION$END» ${GREEN}marcada (Color no. $PICKER).$END"
+				eval "typeset -a ARRAY_${OPCION#*[:.]}[$((${OPCION%[:.]*}-1))]=\"\e[48;5;${PICKER}m\x20\x20\e[m\"";
+				GRAFICAR;
+				echo -e "\n${GREEN}Coordinate «$BOLD$OPCION$END${GREEN}» marked (Color no. $PICKER).$END";	## String
 			elif [[ ${OPCION//[0-9]/} =~ [-_] ]]; then
-				eval "declare -a ARRAY_${OPCION#*[-_]}[$((${OPCION%[-_]*}-1))]=\"[]\""
-				clear; echo; GRAFICAR
-				echo -e "\n${GREEN}Coordenada «$BOLD$OPCION$END» ${GREEN}desmarcada.$END"
+				eval "typeset -a ARRAY_${OPCION#*[-_]}[$((${OPCION%[-_]*}-1))]=\"[]\"";
+				GRAFICAR;
+				echo -e "\n${GREEN}Coordinate «$BOLD$OPCION$END${GREEN}» unmarked.$END";	## String
 			fi
 		fi
 	elif [[ $OPCION =~ ^[qQ]|[eE][xX][iI][tT]$ ]]; then
-		clear; exit 0
+		CLEAR; exit 0
 	elif [[ $OPCION =~ ^[pP]$ ]]; then
-		printf '\e[8;18;145t'; clear; echo
+		printf '\e[8;18;145t';	# Se asegura que la terminal tenga cierto tamaño.
+		CLEAR; echo;
 
 		for COLOR_LOCAL in {0..15}; do
 			printf " \e[7m\e[38;5;${COLOR_LOCAL}m%3s\e[m\e[00m" $COLOR_LOCAL
@@ -181,9 +211,10 @@ while :; do
 			done
 		done; echo
 
+		REDO_PICKER="$PICKER";	# Guarda el color antes de cambiarlo
+
 		while :; do
-			REDO_PICKER="$PICKER"
-			read -rep "$(echo -e "$CYAN${BOLD}Numero del color:$END ")" PICKER
+			read -rep "$(echo -e "$CYAN${BOLD}Color number:$END ")" -i $REDO_PICKER PICKER;	## String
 
 			if [[ $PICKER =~ ^[0-9]+$ && $PICKER -ge 0 && $PICKER -le 255 ]]; then
 				break
@@ -192,31 +223,29 @@ while :; do
 				break
 			fi
 
-			echo -e "${RED}Error: Sólo se aceptan NÚMEROS de 0 a 255.$END"; unset PICKER
+			echo -e "${RED}Only numbers from 0 to 255 are accepted.$END";		## String
+			unset PICKER;
 		done
 
-		clear; echo; GRAFICAR
+		GRAFICAR
 	elif [[ $OPCION =~ ^[Ss]$ ]]; then
-		clear; echo; GRAFICAR
+		GRAFICAR;
 
-		ARCHIVO="$(date +"PixelArt_%a-%b-%d_%H-%M-%S.sh")"
+		ARCHIVO="$(date +"PixelArt_%a-%b-%d_%H-%M-%S.sh")";
 
-		echo -n '#!/usr/bin/env bash'$'\n''echo -e "' > "$ARCHIVO"
+		SAVE_FILE "$ARCHIVO" 2> /dev/null || {
+			while :; do
+				echo -e "${RED}\nFile could not be generated.$END";				## String
+				read -rep "$(echo -e "$CYAN${BOLD}Type the full path to where you want your file to be saved: $END")" -i "$HOME/${ARCHIVO##*/}" ARCHIVO;	## String
 
-		for (( NO_FILA = 1; NO_FILA <= FILAS; NO_FILA++ )); do
-			(for (( NO_COLUMNA = 0; NO_COLUMNA < COLUMNAS; NO_COLUMNA++ )); do
-				printf "%s" "$(eval "echo -n \${ARRAY_$NO_FILA[$NO_COLUMNA]//\[\]/\\\x20\\\x20}")"
-			done) >> "$ARCHIVO"
-			echo -n '\n'  >> "$ARCHIVO"
-		done; echo '"' >> "$ARCHIVO"
+				mkdir -p "${ARCHIVO%/*}";
+				SAVE_FILE "$ARCHIVO" && break;
+			done
+		}
 
-		if [[ -f "$ARCHIVO" ]]; then
-			echo -e "\n${GREEN}Archivo «$ARCHIVO» generado.$END"
-		else
-			echo -e "${RED}Error: El archivo «$ARCHIVO» no sé pudo crear, revise los permisos del directorio.$END"
-		fi
+		echo -e "\n${GREEN}File «$ARCHIVO» generated.$END";						## String
 	elif [[ "$OPCION" == * ]]; then
-		clear; echo; GRAFICAR
-		echo -e "\n${RED}Error: la opción «$OPCION» no existe.$END"
+		GRAFICAR;
+		echo -e "\n${RED}Option «$OPCION» does not exist.$END";					## String
 	fi
 done
